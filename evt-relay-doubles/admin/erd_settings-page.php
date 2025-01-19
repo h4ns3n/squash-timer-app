d73e861dc55db4b6aadae00330b4ca95aee11c7d
Paul Hansen <ph4ns3n@gmail.com>
2025-01-19 15:04:34 +0200

    timer functionality stable but lacks restart capability

--- evt-relay-doubles/admin/erd_settings-page.php
+++ evt-relay-doubles/admin/erd_settings-page.php
@@ -21,6 +21,42 @@ function erd_render_settings_page() {
                         <input type="number" name="erd_settings[timer_font_size]" value="<?php echo esc_attr($settings['timer_font_size'] ?? 20); ?>" min="10" max="200" />
                     </td>
                 </tr>
+                <tr valign="top">
+                    <th scope="row"><?php esc_html_e('Timer Font Color', 'erdct-textdomain'); ?></th>
+                    <td>
+                        <input type="text" name="erd_settings[timer_font_color]" value="<?php echo esc_attr($settings['timer_font_color'] ?? '#000000'); ?>" class="erd-color-picker" />
+                    </td>
+                </tr>
+                <tr valign="top">
+                    <th scope="row"><?php esc_html_e('Message Font Size (pt)', 'erdct-textdomain'); ?></th>
+                    <td>
+                        <input type="number" name="erd_settings[message_font_size]" value="<?php echo esc_attr($settings['message_font_size'] ?? 16); ?>" min="10" max="100" />
+                    </td>
+                </tr>
+                <tr valign="top">
+                    <th scope="row"><?php esc_html_e('Message Font Color', 'erdct-textdomain'); ?></th>
+                    <td>
+                        <input type="text" name="erd_settings[message_font_color]" value="<?php echo esc_attr($settings['message_font_color'] ?? '#000000'); ?>" class="erd-color-picker" />
+                    </td>
+                </tr>
+                <tr valign="top">
+                    <th scope="row"><?php esc_html_e('Gap Between Label and Timer (px)', 'erdct-textdomain'); ?></th>
+                    <td>
+                        <input type="number" name="erd_settings[label_timer_gap]" value="<?php echo esc_attr($settings['label_timer_gap'] ?? 10); ?>" min="-50" max="50" />
+                    </td>
+                </tr>
+                <tr valign="top">
+                    <th scope="row"><?php esc_html_e('Start Time (minutes)', 'erdct-textdomain'); ?></th>
+                    <td>
+                        <input type="number" name="erd_settings[start_time_minutes]" value="<?php echo esc_attr($settings['start_time_minutes'] ?? 0); ?>" min="0" max="120" />
+                    </td>
+                </tr>
+                <tr valign="top">
+                    <th scope="row"><?php esc_html_e('Start Time (seconds)', 'erdct-textdomain'); ?></th>
+                    <td>
+                        <input type="number" name="erd_settings[start_time_seconds]" value="<?php echo esc_attr($settings['start_time_seconds'] ?? 0); ?>" min="0" max="59" />
+                    </td>
+                </tr>
             </table>
 
             <h2><?php esc_html_e('Timer Settings', 'erdct-textdomain'); ?></h2>
@@ -48,6 +84,7 @@ function erd_render_settings_page() {
             <?php submit_button(); ?>
         </form>
     </div>
+    <?php include 'partials/color-picker-settings.php'; ?>
     <?php
 }
 
---
+++ evt-relay-doubles/admin/partials/color-picker-settings.php
@@ -0,0 +1,5 @@
+<script>
+    jQuery(document).ready(function($) {
+        $('.erd-color-picker').wpColorPicker();
+    });
+</script>
