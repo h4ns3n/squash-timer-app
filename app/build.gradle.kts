aaeff8540426be566f4073fd65f7e0b18e1b4101
Paul Hansen <ph4ns3n@gmail.com>
2026-01-29 23:11:51 +0200

    fix: Add missing Ktor ContentNegotiation dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -91,6 +91,7 @@ dependencies {
     implementation("io.ktor:ktor-server-core:2.3.7")
     implementation("io.ktor:ktor-server-netty:2.3.7")
     implementation("io.ktor:ktor-server-websockets:2.3.7")
+    implementation("io.ktor:ktor-server-content-negotiation:2.3.7")
     implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.7")
     
     // JSON Serialization
